4c89d3d220dc1fa5d5ce948eeaa5d943da657ea1
Eric Barnes <ebar1984@gmail.com>
2021-07-22 16:13:04 -0500

    deposit, check balance, change password functions

--- exercises/lesson2/functions.php
+++ exercises/lesson2/functions.php
@@ -39,7 +39,14 @@ function menu() {
 }
 
 function change_password() {
-  // Should open the file and retrieve the users balance
+    $password_changed = FALSE;
+  do {
+      $new_password = readline("Enter New Password>>");
+      echo "Your new password is " . $new_password . ".\n";
+      $_SESSION['account']['password'] = $new_password;
+        $password_changed = TRUE;
+  } while(!$password_changed);
+  menu();
 }
 
 function set_balance() {
@@ -48,6 +55,9 @@ function set_balance() {
 
 function check_balance() {
   // Open up the file and display the balanace
+    $balance = ($_SESSION['account']['balance']);
+    echo "You're balance is currently " . $balance . ".\n";
+    menu();
 }
 
 function withdraw() {
@@ -63,12 +73,12 @@ function withdraw() {
       $balance = ($_SESSION['account']['balance']);
       $withdraw_amount = readline("Amount to Withdraw>>");
 
-      if (is_numeric($withdraw_amount) && $withdraw_amount >= 1) {
+      if (is_numeric($withdraw_amount) && $withdraw_amount >= 1 && $balance >= $withdraw_amount) {
         $_SESSION['account']['balance'] -= $withdraw_amount;
         echo "You're money is being dispensed. You're current balance is " . $_SESSION['account']['balance'] . "!\n";
         $withdraw_complete = TRUE;
       } else {
-        echo "You're cannot withdraw " . $withdraw_amount . ".\n You're current balance is " . $balance . ".\n";
+        echo "You cannot withdraw " . $withdraw_amount . ".\n Your current balance is " . $balance . ".\n";
       }
     } while (!$withdraw_complete);
   }
@@ -78,6 +88,30 @@ function withdraw() {
 
 function deposit() {
   // Asks for the amount of money, does a set_balance
+    $balance = ($_SESSION['account']['balance']);
+
+    if ($balance <= 0) {
+        echo "There is a problem with your account. Speak with representative.";
+    }
+    else {
+        $deposit_complete = FALSE;
+
+        do {
+            $balance = ($_SESSION['account']['balance']);
+            $deposit_amount = readline("Amount to Deposit>>");
+
+            if (is_numeric($deposit_amount) && $deposit_amount >= 1) {
+                $_SESSION['account']['balance'] += $deposit_amount;
+                echo "Thank you for your deposit! You're current balance is now " . $_SESSION['account']['balance'] . "!\n";
+                $deposit_complete = TRUE;
+            } else {
+                echo "Unable to deposit " . $deposit_amount . ".\n You're current balance is " . $balance . ".\n";
+            }
+        } while (!$deposit_complete);
+    }
+
+    menu();
+
 }
 
 function login() {
